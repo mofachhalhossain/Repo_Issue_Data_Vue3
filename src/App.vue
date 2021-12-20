b31f6b66b2993c04df686aa286b79059c51c2fee
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-12-20 20:26:48 +0600

    data received in console

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,68 @@
 <template>
-  <Child />
+  <div id="app">
+    <form action="#" @submit.prevent="getIssues">
+      <div class="form-group">
+        <input
+          type="text"
+          placeholder="owner/repo Name"
+          v-model="repository"
+          class="col-md-2 col-md-offset-5"
+        />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import Child from "./components/Child.vue";
+import moment from "moment";
+import axios from "axios";
 export default {
   name: "App",
-  components: { Child },
+  data() {
+    return {
+      issues: [],
+      repository: "",
+      startDate: null,
+    };
+  },
+  methods: {
+    getIssues() {
+      this.startDate = moment().subtract(6, "days").format("YYYY-MM-DD");
+
+      axios
+        .get(
+          `https://api.github.com/search/issues?q=repo:${this.repository}+is:issue+is:open+created:>=${this.startDate}`,
+          { params: { per_page: 100 } }
+        )
+        .then((response) => {
+          const payload = this.getDateRange();
+
+          response.data.items.forEach((item) => {
+            const key = moment(item.created_at).format("MMM Do YY");
+            const obj = payload.filter((o) => o.day === key)[0];
+            obj.issues += 1;
+          });
+
+          this.issues = payload;
+          console.log(this.issues);
+        });
+    },
+    getDateRange() {
+      const startDate = moment().subtract(6, "days");
+      const endDate = moment();
+      const dates = [];
+
+      while (startDate.isSameOrBefore(endDate)) {
+        dates.push({
+          day: startDate.format("MMM Do YY"),
+          issues: 0,
+        });
+
+        startDate.add(1, "days");
+      }
+      return dates;
+    },
+  },
 };
 </script>
 
